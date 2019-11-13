29ee78950d8c47cf7caaba9acb1e7273f8d6a7eb
Daniel Stoklosa <morormo@gmail.com>
2019-11-13 11:18:20 +0100

    add active

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,21 @@
-	const titleClickHandler = function(event){
-  	console.log('Link was clicked!');
-  	console.log(event);
+const titleClickHandler = function(event){
+  const clickedElement = this;
+  console.log('Link was clicked!');
 
-  /* remove class 'active' from all article links  */
-  	const activeLinks = document.querySelectorAll('.titles a.active');
+  /* [DONE] remove class 'active' from all article links  */
 
-	for(let activeLink of activeLinks){
- 	 activeLink.classList.remove('active');
- 	 console.log('Active was deleted');
-	}
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-  /* add class 'active' to the clicked link */
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
-  /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.post.active');
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-	for(let activeArticle of activeArticles){
- 	 activeArticle.classList.remove('active');
- 	 console.log('Active was deleted');
- 	}
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+
+  /* [DONE] remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
 
@@ -32,64 +29,3 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
-
-/*Link was clicked!
-scripts.js (2,3)
-
-
-
-
-[object PointerEvent]: {altKey: false, bubbles: true, button: 0, buttons: 0, cancelable: true...}
-altKey: false
-bubbles: true
-button: 0
-buttons: 0
-cancelable: true
-cancelBubble: false
-clientX: 92
-clientY: 225
-ctrlKey: false
-
-currentTarget: HTMLAnchorElement
-defaultPrevented: false
-detail: 1
-eventPhase: 3
-fromElement: null
-height: 1
-isPrimary: true
-isTrusted: true
-layerX: 92
-layerY: 225
-metaKey: false
-movementX: 0
-movementY: 0
-offsetX: 33
-offsetY: 23
-pageX: 92
-pageY: 463
-pointerId: 1
-pointerType: "mouse"
-pressure: 0
-relatedTarget: null
-returnValue: true
-screenX: 124
-screenY: 376
-shiftKey: false
-
-srcElement: HTMLSpanElement
-
-target: HTMLSpanElement
-tiltX: 0
-tiltY: 0
-timeStamp: 2568.5819
-toElement: null
-twist: 0
-type: "click"
-
-view: Window
-which: 1
-width: 1
-x: 92
-y: 225
-
-__proto__: PointerEventPrototype*/
