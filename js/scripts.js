03de161085cf6e0445f728fa65db781ff0169f44
Daniel Stoklosa <morormo@gmail.com>
2019-11-14 14:50:55 +0100

    Remove title list scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 'use strict';
-const titleClickHandler = function(event){
+{const titleClickHandler = function(event){
   const clickedElement = this;
   console.log('Link was clicked!');
   
@@ -41,4 +41,31 @@ const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+}
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+const titleList = document.querySelector(optTitleListSelector).innerHTML = '';
+
+  /* for each article */
+ const articles = document.querySelectorAll(optArticleSelector);
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
 }
