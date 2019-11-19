aa0a480e24b491d0bbf0460a44d5ff21c5515224
Morormo <morormo@gmail.com>
2019-11-19 23:15:54 +0100

    Remove list from post-tags

--- js/scripts.js
+++ js/scripts.js
@@ -46,6 +46,7 @@ for(let link of links){
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
+  optArticleTagsSelector = '.post-tags .list'
 
 function generateTitleLinks(){
 
@@ -83,4 +84,36 @@ function generateTitleLinks(){
  }
 
 generateTitleLinks();
+
+function generateTags(){
+
+
+  /* find all articles */
+
+  /* START LOOP: for every article: */
+
+    /* find tags wrapper */
+
+    /* make html variable with empty string */
+
+    /* get tags from data-tags attribute */
+
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+      /* generate HTML of the link */
+
+      /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
 }
+
+generateTags();
+}
+
+
